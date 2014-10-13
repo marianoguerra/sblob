e3129d591aa541133834b163679bb495b64af072
Mariano Guerra <luismarianoguerra@gmail.com>
2014-10-13 10:32:42 +0200

    use ets instead of gb_tree for sblob_preg

--- src/sblob_preg.erl
+++ src/sblob_preg.erl
@@ -1,46 +1,36 @@
 -module(sblob_preg).
--export([new/0, put/3, update/3, get/2, remove/2, to_each/2, foreach/2]).
+-export([new/0, put/3, update/3, get/2, remove/2, foreach/2]).
 
-new() -> gb_trees:empty().
+new() -> ets:new(preg, [protected, ordered_set]).
 
-put(Preg, Key, Val) -> gb_trees:insert(Key, Val, Preg).
-get(Preg, Key) -> gb_trees:lookup(Key, Preg).
-remove(Preg, Key) -> gb_trees:delete(Key, Preg).
-update(Preg, Key, Val) -> gb_trees:update(Key, Val, Preg).
+put(Preg, Key, Val) ->
+    ets:insert(Preg, {Key, Val}),
+    Preg.
 
-% iterates over all key values and updates value with value returned from Fun
-to_each(Preg, Fun) ->
-    Iter = gb_trees:iterator(Preg),
-    to_each(Preg, Fun, Iter).
+get(Preg, Key) ->
+    case ets:lookup(Preg, Key) of
+        [{Key, Val}] -> {value, Val};
+        [] -> none
+    end.
 
-to_each(Preg, _Fun, nil) ->
-    Preg;
+remove(Preg, Key) ->
+    ets:delete(Preg, Key),
+    Preg.
 
-to_each(Preg, Fun, Iter) ->
-    {NewPreg, NewIter} = case gb_trees:next(Iter) of
-                             none -> {Preg, nil};
-                             {Key, Val, Iter1} ->
-                                 NewVal = Fun(Key, Val),
-                                 Preg1 = update(Preg, Key, NewVal),
-                                 {Preg1, Iter1}
-                         end,
-    to_each(NewPreg, Fun, NewIter).
+update(Preg, Key, Val) -> put(Preg, Key, Val).
 
 % iterates over all key values calling Fun for each pair, doesn't change the
 % current value
 foreach(Preg, Fun) ->
-    Iter = gb_trees:iterator(Preg),
-    foreach(Preg, Fun, Iter).
+    Key = ets:first(Preg),
+    foreach(Preg, Fun, Key).
 
-foreach(Preg, _Fun, nil) ->
+foreach(Preg, _Fun, '$end_of_table') ->
     Preg;
 
-foreach(Preg, Fun, Iter) ->
-    NewIter = case gb_trees:next(Iter) of
-                  none -> nil;
-                  {Key, Val, Iter1} ->
-                      Fun(Key, Val),
-                      Iter1
-              end,
-    foreach(Preg, Fun, NewIter).
+foreach(Preg, Fun, Key) ->
+    {value, Val} = get(Preg, Key),
+    Fun(Key, Val),
+    NextKey = ets:next(Preg, Key),
+    foreach(Preg, Fun, NextKey).
 
