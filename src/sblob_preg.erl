3777b4c44d760e0d7d2a7ba3c5a774c55ac31b65
Mariano Guerra <luismarianoguerra@gmail.com>
2014-10-14 09:14:39 +0200

    add reverse support for sblob_preg

--- src/sblob_preg.erl
+++ src/sblob_preg.erl
@@ -1,23 +1,48 @@
 -module(sblob_preg).
--export([new/0, put/3, update/3, get/2, remove/2, foreach/2]).
+-export([new/0, put/3, get/2, get_reverse/2, remove/2, remove_reverse/2, foreach/2]).
 
 new() -> ets:new(preg, [protected, ordered_set]).
 
 put(Preg, Key, Val) ->
-    ets:insert(Preg, {Key, Val}),
+    ets:insert(Preg, {{key, Key}, Val}),
+    ets:insert(Preg, {{val, Val}, Key}),
     Preg.
 
 get(Preg, Key) ->
-    case ets:lookup(Preg, Key) of
-        [{Key, Val}] -> {value, Val};
+    ActualKey = {key, Key},
+    case ets:lookup(Preg, ActualKey) of
+        [{ActualKey, Val}] -> {value, Val};
         [] -> none
     end.
 
+get_reverse(Preg, Val) ->
+    ActualKey = {val, Val},
+    case ets:lookup(Preg, ActualKey) of
+        [{ActualKey, Key}] -> {key, Key};
+        [] -> none
+    end.
+
+remove_both(Preg, Key, Val) ->
+    ets:delete(Preg, {val, Val}),
+    ets:delete(Preg, {key, Key}).
+
 remove(Preg, Key) ->
-    ets:delete(Preg, Key),
+    case get(Preg, Key) of
+        {value, Val} ->
+            remove_both(Preg, Key, Val);
+        none ->
+            ok
+    end,
     Preg.
 
-update(Preg, Key, Val) -> put(Preg, Key, Val).
+remove_reverse(Preg, Val) ->
+    case get_reverse(Preg, Val) of
+        {key, Key} ->
+            remove_both(Preg, Key, Val);
+        none ->
+            ok
+    end,
+    Preg.
 
 % iterates over all key values calling Fun for each pair, doesn't change the
 % current value
@@ -28,7 +53,11 @@ foreach(Preg, Fun) ->
 foreach(Preg, _Fun, '$end_of_table') ->
     Preg;
 
-foreach(Preg, Fun, Key) ->
+foreach(Preg, Fun, Key={val, _Val}) ->
+    NextKey = ets:next(Preg, Key),
+    foreach(Preg, Fun, NextKey);
+
+foreach(Preg, Fun, {key, Key}) ->
     {value, Val} = get(Preg, Key),
     Fun(Key, Val),
     NextKey = ets:next(Preg, Key),
---
+++ test/sblob_preg_tests.erl
@@ -0,0 +1,61 @@
+-module(sblob_preg_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+new() -> sblob_preg:new().
+
+put(Preg, Key, Val) ->
+    sblob_preg:put(Preg, Key, Val).
+
+get(Preg, Key) ->
+    sblob_preg:get(Preg, Key).
+
+getr(Preg, Val) ->
+    sblob_preg:get_reverse(Preg, Val).
+
+del(Preg, Key) ->
+    sblob_preg:remove(Preg, Key).
+
+delr(Preg, Val) ->
+    sblob_preg:remove_reverse(Preg, Val).
+
+put_get_reverse_test() ->
+    Preg = new(),
+    put(Preg, mykey, myval),
+    Res = get(Preg, mykey),
+    ResRev = getr(Preg, myval),
+    ?assertEqual({value, myval}, Res),
+    ?assertEqual({key, mykey}, ResRev).
+
+put_get_reverse_del_test() ->
+    Preg = new(),
+    put(Preg, mykey, myval),
+    Res = get(Preg, mykey),
+    ResRev = getr(Preg, myval),
+    del(Preg, mykey),
+    Res1 = get(Preg, mykey),
+    ResRev1 = getr(Preg, myval),
+    ?assertEqual({value, myval}, Res),
+    ?assertEqual({key, mykey}, ResRev),
+    ?assertEqual(none, Res1),
+    ?assertEqual(none, ResRev1).
+
+put_get_reverse_del_reverse_test() ->
+    Preg = new(),
+    put(Preg, mykey, myval),
+    put(Preg, otherkey, otherval),
+    Res = get(Preg, mykey),
+    ResRev = getr(Preg, myval),
+    delr(Preg, myval),
+    OtherRes = get(Preg, otherkey),
+    OtherResRev = getr(Preg, otherval),
+    Res1 = get(Preg, mykey),
+    ResRev1 = getr(Preg, myval),
+    ?assertEqual({value, myval}, Res),
+    ?assertEqual({key, mykey}, ResRev),
+
+    ?assertEqual({value, otherval}, OtherRes),
+    ?assertEqual({key, otherkey}, OtherResRev),
+
+    ?assertEqual(none, Res1),
+    ?assertEqual(none, ResRev1).
+
